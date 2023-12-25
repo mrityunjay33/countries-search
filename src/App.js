5aef727cfb1348a934f576244971666d2cd7ad7f
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-25 21:41:38 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <div className='container'>
+      <h1>Search Country</h1>
+    </div>
+  );
+}
+
+export default App;
