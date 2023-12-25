66d96f7eebac035569bccbbce04538fd75c9ec48
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-25 22:06:39 +0530

    Feat: Debouncing with timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
- 
+
+  const [flags, setFlags] = useState([]);
+  const [flagsAllData, setFlagsAllData] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [timerId, setTimerId] = useState(null);
+
+  useEffect(()=>{
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try{
+      const res = await fetch('https://restcountries.com/v3.1/all');
+      const data = await res.json();
+      setFlags(data);
+      setFlagsAllData(data);
+    }
+    catch(err){
+      console.log(err);
+      return [];
+    }
+  }
+
+  const handleSearch = (e) => {
+    const searchText = e.target.value;
+    setSearchText(searchText);
+    if(timerId) clearTimeout(timerId);
+
+    const timer = setTimeout(()=>{
+      const filterFlags  = flagsAllData.filter((data) => {
+        return data.region.toLowerCase().includes(searchText.toLowerCase());
+      })
+      setFlags(filterFlags);
+    },1000);
+    
+    setTimerId(timer);
+  }
 
   return (
+    <>
+      <input type='text' placeholder='Type to Search' value={searchText} onChange={handleSearch}/>
     <div className='container'>
-      <h1>Search Country</h1>
+      {flags.map((data, idx) => <div key={idx}>
+        <div className='card'>
+          <div className='card-content'>
+          <img className='image' src={data.flags.png} alt={`Flag of ${data.name.common}`} />
+          <div className='country-name'>{data.region}</div>
+          </div>
+        </div>
+        </div>)}
     </div>
+    </>
   );
 }
 
